ac600b7f26f70b91ce524d314c5c9d1fea5fc4cd
Pascal Welsch <pascal@welsch.dev>
2023-02-01 14:07:56 +0100

    Update to sidekick_core 1.0.0

--- lib/src/util/git_utils.dart
+++ lib/src/util/git_utils.dart
@@ -1,13 +1,15 @@
 // ignore_for_file: implementation_imports, depend_on_referenced_packages
-
+import 'package:dcli/dcli.dart' as dcli;
 import 'package:sidekick_core/sidekick_core.dart';
 
 extension LocalChanges on File {
   bool hasLocalChanges() {
-    return git(['diff', '--exit-code', absolute.path]) == 1;
+    final progress =
+        dcli.startFromArgs('git', ['diff', '--exit-code', absolute.path]);
+    return progress.exitCode == 1;
   }
 
   void discardLocalChanges() {
-    git(['checkout', absolute.path]);
+    'git checkout ${absolute.path}'.run;
   }
 }
