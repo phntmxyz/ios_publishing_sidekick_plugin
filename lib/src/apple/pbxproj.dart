223a1007d52bc8f80782b50e1380e6b6b1710a7f
Pascal Welsch <pascal@phntm.xyz>
2024-11-05 16:37:03 +0100

    Add new XcodePbxproj methods: setCodeSignStyle and setDevelopmentTeam

--- lib/src/apple/pbxproj.dart
+++ lib/src/apple/pbxproj.dart
@@ -41,6 +41,42 @@ class XcodePbxproj {
     );
     file.writeAsStringSync(update);
   }
+
+  /// Changes CODE_SIGN_STYLE of all configurations
+  void setCodeSignStyle(String style) {
+    file.verifyExistsOrThrow();
+
+    print('Changing CODE_SIGN_STYLE to "$style"');
+    final content = file.readAsStringSync();
+    final bundleIdentifierRegex = RegExp('CODE_SIGN_STYLE =.*;');
+    final match = bundleIdentifierRegex.hasMatch(content);
+    if (!match) {
+      throw "project.pbxproj doesn't contain 'CODE_SIGN_STYLE'";
+    }
+    final update = content.replaceAll(
+      bundleIdentifierRegex,
+      'CODE_SIGN_STYLE = $style;',
+    );
+    file.writeAsStringSync(update);
+  }
+
+  /// Changes DEVELOPMENT_TEAM of all configurations
+  void setDevelopmentTeam(String team) {
+    file.verifyExistsOrThrow();
+
+    print('Changing DEVELOPMENT_TEAM to "$team"');
+    final content = file.readAsStringSync();
+    final bundleIdentifierRegex = RegExp('DEVELOPMENT_TEAM =.*;');
+    final match = bundleIdentifierRegex.hasMatch(content);
+    if (!match) {
+      throw "project.pbxproj doesn't contain 'DEVELOPMENT_TEAM'";
+    }
+    final update = content.replaceAll(
+      bundleIdentifierRegex,
+      'DEVELOPMENT_TEAM = $team;',
+    );
+    file.writeAsStringSync(update);
+  }
 }
 
 extension XcodePbxprojFile on File {
