888336bd66adfa928f0e183ff9fcbb41cee5f39d
Pascal Welsch <pascal@phntm.xyz>
2025-04-03 13:39:24 +0200

    Deprecate ad-hoc, add release-testing

--- lib/src/apple/export_options.dart
+++ lib/src/apple/export_options.dart
@@ -2,14 +2,24 @@
 ///
 /// Describes how Xcode should export the archive.
 enum ExportMethod {
+  @Deprecated('Use releaseTesting instead')
   adHoc('ad-hoc'),
+
+  releaseTesting('release-testing'),
+
   @Deprecated('Use appStoreConnect instead')
   appStore('app-store'),
+
   appStoreConnect('app-store-connect'),
+
   validation('validation'),
+
   package('package'),
+
   enterprise('enterprise'),
+
   developerId('developer-id'),
+
   macApplication('mac-application');
 
   final String value;
